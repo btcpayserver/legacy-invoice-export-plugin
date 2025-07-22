e82bddde82fff85886899939d8c4465bd0548141
nicolas.dorier <nicolas.dorier@gmail.com>
2025-07-22 12:10:33 +0900

    Init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Motivation
+
+This plugin enables access to the `Legacy Invoice Report` from the `Reporting` page in BTCPay Server.
+
+Until version 2.1.6, the `Legacy Invoice Report` was part of the core. Starting with version 2.1.7, the report was replaced by the updated `Invoice Report`, which includes additional columns and some renamed fields.
+
+While we recommend using the new `Invoice Report`, this plugin is provided to maintain compatibility with existing workflows that rely on the legacy version.
---
+++ src/LegacyInvoiceExport/LegacyInvoiceExportPlugin.cs
@@ -0,0 +1,19 @@
+﻿using BTCPayServer.Abstractions.Contracts;
+using BTCPayServer.Abstractions.Models;
+using BTCPayServer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LegacyInvoiceExport;
+
+public class LegacyInvoiceExportPlugin : BaseBTCPayServerPlugin
+{
+    public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
+    {
+        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=2.1.7" }
+    };
+
+    public override void Execute(IServiceCollection services)
+    {
+        services.AddReportProvider<LegacyInvoiceExportReportProvider>();
+    }
+}
